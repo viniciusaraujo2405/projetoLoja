f44cb3a30e008ce640a2dd1932859d56ce5f8605
Vinícius Araújo <d2022003334@unifei.edu.br>
2025-01-14 14:30:20 -0300

    Criando a pasta pages e criando o arquivo products_overview

---
+++ pages/products_overview_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+class ProductsOverviewPage extends StatelessWidget{
+  const ProductsOverviewPage ({Key ? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Container();
+  }
+
+
+}
