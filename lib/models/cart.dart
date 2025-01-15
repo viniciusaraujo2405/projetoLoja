25d82cd1c8602d16a8f0b74aaa7ee184bf62c4b1
Vinícius Araújo <d2022003334@unifei.edu.br>
2025-01-15 17:49:23 -0300

    Criando as classses cart e cart_item com seus atributos e métodos para implementar o carrinho de compras

---
+++ lib/models/cart.dart
@@ -0,0 +1,62 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:shop/models/cart_item.dart';
+import 'package:shop/models/product.dart';
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _items = {};
+
+  Map<String, CartItem> get items {
+    return {..._items};
+  }
+
+  int get itemCount {
+    return _items.length;
+  }
+
+  double get totalAmount {
+    var total = 0.0;
+    _items.forEach((key, cartItem) {
+      total += cartItem.price * cartItem.quantity;
+    });
+    return total;
+  }
+
+  void addItem(Product product) {
+    if (_items.containsKey(product.id)) {
+      _items.update(
+        product.id,
+        (existingCartItem) => CartItem(
+          id: existingCartItem.id,
+          name: existingCartItem.name,
+          quantity: existingCartItem.quantity + 1,
+          price: existingCartItem.price,
+          productId: existingCartItem.productId,
+        ),
+      );
+    } else {
+      _items.putIfAbsent(
+        product.id,
+        () => CartItem(
+          id: Random().nextDouble().toString(),
+          name: product.title,
+          quantity: 1,
+          price: product.price,
+          productId: product.id,
+        ),
+      );
+    }
+    notifyListeners();
+  }
+
+  void removeItem(String productId) {
+    _items.remove(productId);
+    notifyListeners();
+  }
+
+  void clear(){
+    _items = {};
+    notifyListeners();
+  }
+}
