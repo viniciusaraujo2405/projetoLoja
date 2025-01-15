8dec037daf557c6b4e2c7a71d51c4023161de9e6
Vinícius Araújo <d2022003334@unifei.edu.br>
2025-01-15 11:49:31 -0300

    Criando a classe productList e implementando o provider

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -1,7 +1,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:shop/models/product.dart';
-import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/utils/app_routes.dart';
 
 class ProductItem extends StatelessWidget {
--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/components/product_item.dart';
-import 'package:shop/data/dummy_data.dart';
 import 'package:shop/models/product.dart';
+import 'package:shop/models/product_list.dart';
 
 class ProductsOverviewPage extends StatelessWidget{
-  final List<Product> loadedProducts = dummyProducts;
   ProductsOverviewPage ({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context){
+    final provider = Provider.of<ProductList>(context);
+    final List<Product> loadedProducts = provider.items;
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
