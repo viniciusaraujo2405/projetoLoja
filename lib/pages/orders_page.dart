96a9af77021d5740df79e7cf77144cc82a52dca0
Vinícius Araújo <d2022003334@unifei.edu.br>
2025-01-16 20:41:22 -0300

    Criando a tela pros pedidos

---
+++ lib/pages/orders_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class OrderPage extends StatelessWidget {
+  const OrderPage({Key? key}) : super(key: key);
+
+  @override
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Meus pedidos'),
+      ),
+      body: Center(
+        child: Text('Orders'),
+      ),
+    );
+  }
+}
