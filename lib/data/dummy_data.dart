02bc3958648c145bb509a00f9f877bbf99795ee5
Vinícius Araújo <d2022003334@unifei.edu.br>
2025-01-17 11:05:50 -0300

    Alterando titulo e descricao dos produtos e criando um produto p6

--- lib/data/dummy_data.dart
+++ lib/data/dummy_data.dart
@@ -4,32 +4,32 @@ final dummyProducts = [
   
     Product(
       id: 'p1',
-      title: 'Red Shirt',
-      description: 'A red shirt - it is pretty red!',
+      title: 'Camisa vermelha',
+      description: 'Camisa vermelha básica',
       price: 29.99,
       imageUrl:
           'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
     ),
     Product(
       id: 'p2',
-      title: 'Trousers',
-      description: 'A nice pair of trousers.',
+      title: 'Calça',
+      description: 'Uma calça confortável para o dia a dia.',
       price: 59.99,
       imageUrl:
           'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
     ),
     Product(
       id: 'p3',
-      title: 'Yellow Scarf',
-      description: 'Warm and cozy - exactly what you need for the winter.',
+      title: 'Cachecol Amarelo',
+      description: 'Quente e confortável - Exatamente o que você precisa para o inverno.',
       price: 19.99,
       imageUrl:
           'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
     ),
     Product(
       id: 'p4',
-      title: 'A Pan',
-      description: 'Prepare any meal you want.',
+      title: 'Panela',
+      description: 'Prepare qualquer refeição que você queria com essa panela.',
       price: 49.99,
       imageUrl:
           'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
@@ -37,12 +37,21 @@ final dummyProducts = [
 
     Product(
       id: 'p5',
-      title: 'A Nike Shoe',
-      description: 'A nice pair of Nike shoe.',
+      title: 'Tênis Nike',
+      description: 'Uma ótima opção de tênis para o dia a dia.',
       price: 99.99,
       imageUrl: 
           'https://th.bing.com/th/id/OPHS.55HutwNlxWqeNQ474C474?o=5&pid=21.1&w=160&h=160'
     ),
+    Product(
+      id: 'p6',
+      title: 'Camisa do Corinthians',
+      description: 'A Camisa do melhor time do mundo.',
+      price: 332.49,
+      imageUrl: 
+          'https://imgnike-a.akamaihd.net/360x360/02870551A8.jpg'
+    ),
     
+
   
 ];
