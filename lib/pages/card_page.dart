4f2fa5c04cd7964a85d096e59928fdb58837e44e
Vinícius Araújo <d2022003334@unifei.edu.br>
2025-01-18 18:38:51 -0300

    Desenvolvendo a pagina dos cartoes

--- lib/pages/card_page.dart
+++ lib/pages/card_page.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/material.dart';
 
-class CardPage extends StatelessWidget {
+class CardPage extends StatefulWidget {
   const CardPage({Key? key}) : super(key: key);
+
+  @override
+  State<CardPage> createState() => _CardPageState();
+}
+
+class _CardPageState extends State<CardPage> {
   @override
   Widget build(BuildContext context) {
+
+    List<Map<String,String>> cartoes = [];
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Meus cartões"),
       ),
-      body: Center(
-        child: Text("Meus cartões"),
+      body: cartoes.isEmpty ? Center(
+        child: Text("Nenhum cartão cadastrado."))
+        : ListView.builder(
+          itemCount: cartoes.length,
+          itemBuilder:(context,index){
+            final cartao = cartoes[index];
+            return ListTile(
+              title: Text(cartao['nome'] ?? ''),
+              subtitle: Text('**** **** **** ${cartao['numero']?.substring(cartao['numero']!.length - 4)}'),
+            );
+          },
+        ),
       ),
-    );
+      );
+    }
   }
-}
